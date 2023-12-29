61cd262adede2c7fb82bdb2bc557a4c9e6947b52
Ghizlane Tahiri Zainane <44745425+ghizlanetahiri95@users.noreply.github.com>
2023-12-29 17:39:22 +0100

    Update file-cut.sh

--- file-cut.sh
+++ file-cut.sh
@@ -1,21 +1,28 @@
+
 #!/bin/bash
 
-# Calcular la proporción para el alumno30
-proportion=$(echo "scale=2; 1 / 30" | bc)
+# Número de alumno
+alumno_number=30
 
-# Obtener el nombre de los ficheros en el directorio actual
-files=$(ls *.fastq)
+# Obtener la ruta completa de los archivos en el directorio actual
+files=$(ls /home/alumno30/lab4_git/*.fastq)
 
 # Iterar sobre cada fichero y cortar la parte superior según la proporción
 for file in $files; do
-    # Nombre del nuevo fichero con la parte superior cortada
-    new_file="cut_${file}"
+    # Obtener el nombre del archivo sin la ruta
+    file_name=$(basename $file)
 
-    # Calcular el número de líneas a cortar
-    lines_to_cut=$(echo "$(wc -l $file | awk '{print $1}') * $proportion" | bc | cut -f1 -d.)
+    # Nombre del nuevo fichero con la parte superior cortada
+    new_file="/home/alumno30/lab4_git/cut_${file_name}"
+
+    # Calcular el número de líneas a cortar y redondear hacia abajo
+    lines_to_cut=$(echo "$(wc -l $file | awk '{print int($1 * $alumno_number)}')" | bc)
 
     # Cortar y guardar la parte superior en el nuevo fichero
     head -n $lines_to_cut $file > $new_file
 
-    echo "Archivo $file cortado y guardado en $new_file"
+    echo "Archivo $file_name cortado y guardado en $new_file"
 done
+
+
+
